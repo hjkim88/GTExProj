e94ab7ffd2ec30c3e7d0d60fa79153f258243cca
hjkim88 <firadazer@gmail.com>
2018-12-19 17:03:13 -0500

    PreprocessTCGA.R - initial commit We downloaded raw counts of all the TCGA tissues and now we would like to organize them in one RDA file with sample information including RIN.

---
+++ codes/PreprocessTCGA.R
@@ -0,0 +1,26 @@
+###
+#   File name : PreprocessTCGA.R
+#   Author    : Hyunjin Kim
+#   Date      : Dec 19, 2018
+#   Email     : hk2990@cumc.columbia.edu
+#   Purpose   : We downloaded raw counts of all the TCGA tissues and now we would like to organize them
+#               in one RDA file with sample information including RIN
+#
+#   Instruction
+#               1. Source("PreprocessTCGA.R")
+#               2. Run the function "preprocess_tcga()" - specify the target directory and output file path
+#               3. The preprocessed RDA file will be generated in the output file path
+#
+#   Example
+#               > source("The_directory_of_PreprocessTCGA.R/PreprocessTCGA.R")
+#               > preprocess_tcga()
+###
+
+preprocess_tcga <- function() {
+  
+  
+  
+  
+  
+}
+
