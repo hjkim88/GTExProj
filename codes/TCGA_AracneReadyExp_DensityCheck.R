c5d1963b3a7ec0c1aecdd00945ec72e7ec0a5d33
hjkim88 <firadazer@gmail.com>
2019-01-11 17:34:59 -0500

    now checking depth of coverage (total number of reads) of the deviating samples - added.

--- codes/TCGA_AracneReadyExp_DensityCheck.R
+++ codes/TCGA_AracneReadyExp_DensityCheck.R
@@ -14,18 +14,36 @@
 #
 #   Example
 #               > source("The_directory_of_TCGA_AracneReadyExp_DensityCheck.R/TCGA_AracneReadyExp_DensityCheck.R")
-#               > densityCheck(AracneReadyRDAPath="./data/RDA_Files/TCGA_26_ARACNE_READY_EXPMAT.rda")
+#               > densityCheck(AracneReadyRDAPath="./data/RDA_Files/TCGA_26_ARACNE_READY_EXPMAT.rda",
+#                              rawCntRDAPath="./data/RDA_Files/TCGA_33_RAW_COUNTS.rda")
 ###
 
-densityCheck() <- function(AracneReadyRDAPath="./data/RDA_Files/TCGA_26_ARACNE_READY_EXPMAT.rda") {
+densityCheck() <- function(AracneReadyRDAPath="./data/RDA_Files/TCGA_26_ARACNE_READY_EXPMAT.rda",
+                           rawCntRDAPath="./data/RDA_Files/TCGA_33_RAW_COUNTS.rda") {
+  
+  ### load library
+  if(!require(ggplot2)) {
+    install.packages("ggplot2")
+    library(ggplot2)
+  }
+  if(!require(ggbeeswarm)) {
+    install.packages("ggbeeswarm")
+    library(ggbeeswarm)
+  }
+  if(!require(ggpubr)) {
+    install.packages("ggpubr")
+    library(ggpubr)
+  }
+  
   
   ### load data
+  load(rawCntRDAPath)
   load(AracneReadyRDAPath)
   
   
   ### print density plots
   for(i in 1:length(tcga_expmat_names)) {
-    mat = get(tcga_expmat_names[i])
+    mat <- get(tcga_expmat_names[i])
     plot(density(mat[,1]), main = tcga_expmat_names[i])
     apply(mat[, 2:ncol(mat)], 2, function(x){lines(density(x))})
   }
@@ -35,39 +53,72 @@ densityCheck() <- function(AracneReadyRDAPath="./data/RDA_Files/TCGA_26_ARACNE_R
   ### we want to know what they are
   
   ### TCGA_ESCA
-  tum_no <- 5
-  mat = get(tcga_expmat_names[tum_no])
   
   ### variances check
-  variances <- apply(mat, 2, var)
+  variances <- apply(expmat_tcga_esca, 2, var)
   barplot(variances, main = tcga_expmat_names[tum_no])
   
   ### deviating samples have relatively low variances than the others
-  plot(density(mat[,1]), main = tcga_expmat_names[tum_no])
-  apply(mat[, 2:ncol(mat)], 2, function(x){lines(density(x))})
-  apply(mat[, order(variances)[1:10]], 2, function(x){lines(density(x), col = "RED")})
+  plot(density(expmat_tcga_esca[,1]), main = tcga_expmat_names[tum_no])
+  apply(expmat_tcga_esca[, 2:ncol(expmat_tcga_esca)], 2, function(x){lines(density(x))})
+  apply(expmat_tcga_esca[, order(variances)[1:10]], 2, function(x){lines(density(x), col = "RED")})
   
   ### compare sample info between deviating and normal samples
-  target_sample_info <- tcga_sample_info[colnames(mat)[order(variances)[1:10]],]
-  esca_sample_info <- tcga_sample_info[colnames(mat),]
+  target_sample_info <- tcga_sample_info[colnames(expmat_tcga_esca)[order(variances)[1:10]],]
+  esca_sample_info <- tcga_sample_info[colnames(expmat_tcga_esca),]
+  
+  
+  ### now check depth of coverage (total number of reads) of the deviating samples
+  target_no.reads <- apply(rcnt_tcga_esca[,target_sample_info[,"barcode"]], 2, sum)
+  other_no.reads <- apply(rcnt_tcga_esca[,setdiff(esca_sample_info[,"barcode"],
+                                                  target_sample_info[,"barcode"])], 2, sum)
+  avg.no.reads <- mean(c(target_no.reads, other_no.reads))
+  
+  ### beeswarm plot
+  df <- data.frame(Type=c(rep("Deviating_Samples", length(target_no.reads)),
+                          rep("The_Others", length(other_no.reads))),
+                   No_Reads=c(target_no.reads, other_no.reads),
+                   stringsAsFactors = FALSE, check.names = FALSE)
+  ggplot(df, aes(x=Type, y=No_Reads)) +
+    labs(title="TCGA_ESCA", y="Total number of reads") +
+    theme_classic(base_size = 16) +
+    geom_boxplot() +
+    geom_beeswarm(aes(color=Type)) +
+    stat_compare_means()
   
   
   ### TCGA_STAD
-  tum_no <- 22
-  mat = get(tcga_expmat_names[tum_no])
   
   ### variances check
-  variances <- apply(mat, 2, var)
+  variances <- apply(expmat_tcga_stad, 2, var)
   barplot(variances, main = tcga_expmat_names[tum_no])
   
   ### deviating samples have relatively low variances than the others
-  plot(density(mat[,1]), main = tcga_expmat_names[tum_no])
-  apply(mat[, 2:ncol(mat)], 2, function(x){lines(density(x))})
-  apply(mat[, order(variances)[1:20]], 2, function(x){lines(density(x), col = "RED")})
+  plot(density(expmat_tcga_stad[,1]), main = tcga_expmat_names[tum_no])
+  apply(expmat_tcga_stad[, 2:ncol(expmat_tcga_stad)], 2, function(x){lines(density(x))})
+  apply(expmat_tcga_stad[, order(variances)[1:20]], 2, function(x){lines(density(x), col = "RED")})
   
   ### compare sample info between deviating and normal samples
-  target_sample_info <- tcga_sample_info[colnames(mat)[order(variances)[1:20]],]
-  stad_sample_info <- tcga_sample_info[colnames(mat),]
+  target_sample_info <- tcga_sample_info[colnames(expmat_tcga_stad)[order(variances)[1:20]],]
+  stad_sample_info <- tcga_sample_info[colnames(expmat_tcga_stad),]
+  
+  ### now check depth of coverage (total number of reads) of the deviating samples
+  target_no.reads <- apply(rcnt_tcga_stad[,target_sample_info[,"barcode"]], 2, sum)
+  other_no.reads <- apply(rcnt_tcga_stad[,setdiff(stad_sample_info[,"barcode"],
+                                                  target_sample_info[,"barcode"])], 2, sum)
+  avg.no.reads <- mean(c(target_no.reads, other_no.reads))
+  
+  ### beeswarm plot
+  df <- data.frame(Type=c(rep("Deviating_Samples", length(target_no.reads)),
+                          rep("The_Others", length(other_no.reads))),
+                   No_Reads=c(target_no.reads, other_no.reads),
+                   stringsAsFactors = FALSE, check.names = FALSE)
+  ggplot(df, aes(x=Type, y=No_Reads)) +
+    labs(title="TCGA_STAD", y="Total number of reads") +
+    theme_classic(base_size = 16) +
+    geom_boxplot() +
+    geom_beeswarm(aes(color=Type)) +
+    stat_compare_means()
   
   
   ### Concluded that both in TCGA_ESCA and TCGA_STAD, there are no difference in sample info
