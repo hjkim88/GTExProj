68a86676f30753c7ad779347b3566fcd51e3cdf8
hjkim88 <firadazer@gmail.com>
2019-12-26 13:23:36 -0500

    skeleton of GraphKernelRCPP.R

---
+++ codes/GraphKernelRCPP.R
@@ -0,0 +1,29 @@
+###
+#   File name : GraphKernelRCPP.R
+#   Author    : Hyunjin Kim
+#   Date      : Dec 26, 2019
+#   Email     : hk2990@cumc.columbia.edu
+#   Purpose   : We would like to run graph kernel functions in the "graphkernels" package,
+#               but we want to modify some of the CPP code of it. So, we are testing
+#               the new function with the modified CPP code.
+#
+#   Instruction
+#               1. Source("GraphKernelRCPP.R")
+#               2. Run the function "geometricRandomWalk" - specify the necessary input and output paths
+#               3. The result file will be generated in the output path
+#
+#   Example
+#               > source("The_directory_of_GraphKernelRCPP.R/GraphKernelRCPP.R")
+#               > geometricRandomWalk()
+###
+
+geometricRandomWalk <- function() {
+  
+  
+  
+  
+  
+}
+
+
+
