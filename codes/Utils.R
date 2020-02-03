067804a7f31ca8838b41bcf8d38e28112fd149f4
hjkim88 <firadazer@gmail.com>
2020-02-03 15:26:53 -0500

    pca_plot(), pca_plot(), and normalizeRNASEQwithTPM() newly added.

--- codes/Utils.R
+++ codes/Utils.R
@@ -1707,6 +1707,160 @@ mdsPlot <- function(mat, plot_names = FALSE, alt_names = NULL, groups = NULL, di
 }
 
 
+### A function to perform 2D PCA and save a plot
+### normalizedMat: rows are genes and columns are samples
+### grp: group information of the samples
+### num: the number of top genes to be used based on variance (-1 [default]: use all the genes)
+### component: to draw a plot with PC1 & PC2 or PC2 & PC3
+### title: title of the plot
+### suppliment: if it is TRUE, this function additionally generates figures and tables
+###             related to contributions. For example, which genes are highly contributed to
+###             the PC1, PC2, etc.
+### outDir: output directory for the plot
+pca_plot <- function(normalizedMat, grp,
+                     num = -1, component=c("PC1&PC2", "PC2&PC3"),
+                     title="PCA_Plot",
+                     suppliment=FALSE,
+                     outDir="./") {
+  ### load library
+  if(!require(ggfortify, quietly = TRUE)) {
+    install.packages("ggfortify")
+    library(ggfortify, quietly = TRUE)
+  }
+  if(!require(FactoMineR, quietly = TRUE)) {
+    install.packages("FactoMineR")
+    library(FactoMineR, quietly = TRUE)
+  }
+  if(!require(factoextra, quietly = TRUE)) {
+    install.packages("factoextra")
+    library(factoextra, quietly = TRUE)
+  }
+  if(!require(xlsx, quietly = TRUE)) {
+    install.packages("xlsx")
+    require(xlsx, quietly = TRUE)
+  }
+  
+  ### select the top genes based on variance
+  if(num >= 0 && num <= nrow(normalizedMat)) {
+    v <- apply(normalizedMat, 1, var)
+    v <- v[order(-v)]
+    top_genes <- names(v)[1:num]
+  } else {
+    top_genes <- rownames(normalizedMat)
+  }
+  
+  ### PCA
+  pca_result <- PCA(t(normalizedMat[top_genes,]), graph = FALSE)
+  colnames(pca_result$ind$coord) <- paste0("PC", 1:ncol(pca_result$ind$coord))
+  colnames(pca_result$var$contrib) <- paste0("PC", 1:ncol(pca_result$var$contrib))
+  pca_group <- data.frame(pca_result$ind$coord, group=grp)
+  
+  colors = rainbow(length(unique(grp)))
+  names(colors) = unique(grp)
+  
+  ### save as png
+  if(component[1] == "PC1&PC2") {
+    ggplot(pca_group,aes(x=PC1,y=PC2,col=group)) +
+      labs(title=paste0(title, "_PC1-2")) +
+      geom_text(aes(label=colnames(normalizedMat)),hjust="inward", vjust="inward") +
+      scale_color_manual(values = colors) +
+      theme_classic(base_size = 16)
+    ggsave(filename = paste0(outDir, title, "_PC1-2", ".png"), width = 10, height = 8)
+    
+    if(suppliment) {
+      fviz_contrib(pca_result, choice = "var", axes = 1, top = 30)
+      ggsave(filename = paste0(outDir, title, "_PC1_contribution.png"), width = 12, height = 8)
+      
+      fviz_contrib(pca_result, choice = "var", axes = 2, top = 30)
+      ggsave(filename = paste0(outDir, title, "_PC2_contribution.png"), width = 12, height = 8)
+    }
+  } else if(component[1] == "PC2&PC3") {
+    ggplot(pca_group,aes(x=PC2,y=PC3,col=group)) +
+      labs(title=paste0(title, "_PC2-3")) +
+      geom_text(aes(label=colnames(normalizedMat)),hjust="inward", vjust="inward") +
+      scale_color_manual(values = colors) +
+      theme_classic(base_size = 16)
+    ggsave(filename = paste0(outDir, title, "_PC2-3", ".png"), width = 10, height = 8)
+    
+    if(suppliment) {
+      fviz_contrib(pca_result, choice = "var", axes = 2, top = 30)
+      ggsave(filename = paste0(outDir, title, "_PC2_contribution.png"), width = 12, height = 8)
+      
+      fviz_contrib(pca_result, choice = "var", axes = 3, top = 30)
+      ggsave(filename = paste0(outDir, title, "_PC3_contribution.png"), width = 12, height = 8)
+    }
+  } else {
+    stop("\"component\" parameter should be \"PC1&PC2\" or \"PC2&PC3\"")
+  }
+  
+  if(suppliment) {
+    write.xlsx2(data.frame(Gene_Symbol=rownames(pca_result$var$contrib), pca_result$var$contrib,
+                           stringsAsFactors = FALSE, check.names = FALSE),
+                file = paste0(outDir, title, "_PC_contribution.xlsx"),
+                sheetName = "PCA_contribution", row.names = FALSE)
+  }
+}
+
+
+### A function to perform t-SNE and save a plot
+### normalizedMat: rows are genes and columns are samples
+### grp: group information of the samples
+### num: the number of top genes to be used based on variance (-1 [default]: use all the genes)
+### title: title of the plot
+### outDir: output directory for the plot
+tsne_plot <- function(normalizedMat, grp, num = -1, title="TSNE_Plot", outDir="./") {
+  ### load library
+  if(!require(Rtsne)) {
+    install.packages("Rtsne")
+    library(Rtsne)
+  }
+  
+  ### select the top genes based on variance
+  if(num >= 0 && num <= nrow(normalizedMat)) {
+    v <- apply(normalizedMat, 1, var)
+    v <- v[order(-v)]
+    top_genes <- names(v)[1:num]
+  } else {
+    top_genes <- rownames(normalizedMat)
+  }
+  
+  ### TSNE
+  set.seed(1234)
+  t <- tryCatch({
+    writeLines("Perplexity = 30")
+    return(Rtsne(t(normalizedMat[top_genes,]), perplexity = 30))
+  }, error = function(err) {
+    t <- tryCatch({
+      writeLines("Perplexity = 10")
+      return(Rtsne(t(normalizedMat[top_genes,]), perplexity = 10))
+    }, error = function(err) {
+      t <- tryCatch({
+        writeLines("Perplexity = 5")
+        return(Rtsne(t(normalizedMat[top_genes,]), perplexity = 5))
+      }, error = function(err) {
+        t <- tryCatch({
+          writeLines("Perplexity = 3")
+          return(Rtsne(t(normalizedMat[top_genes,]), perplexity = 3))
+        }, error = function(err) {
+          writeLines("Perplexity = 2")
+          return(Rtsne(t(normalizedMat[top_genes,]), perplexity = 2))
+        })
+      })
+    })
+  })
+  
+  colors = rainbow(length(unique(grp)))
+  names(colors) = unique(grp)
+  
+  ### save a plot
+  png(filename=paste0(outDir, title, ".png"), width = 1000, height = 800)
+  plot(t$Y, col="white", xlab="tsne1", ylab="tsne2", main = title)
+  text(t$Y, labels=colnames(normalizedMat), col=colors[grp])
+  legend("topleft", legend = unique(grp), col = colors[unique(grp)], pch = 15)
+  dev.off()
+}
+
+
 # ******************************************************************************
 # Method to read RNA-seq read counts
 #
@@ -2178,6 +2332,13 @@ append.Rda <- function(vars, file_name, overwrite = FALSE) {
 
 #'******************************************************************************
 #' A function to transform RNA-Seq data with VST in DESeq2 package
+#' readCount: RNA-Seq rawcounts in a matrix or in a data frame form
+#'            Rows are genes and columns are samples
+#' filter_thresh: The function filters out genes that have at least one sample
+#'                with counts larger than the 'filter_thresh' value
+#'                e.g., if the 'filter_thresh' = 1, then it removes genes
+#'                that have counts <= 1 across all the samples
+#'                if 0, then there will be no filtering
 #'******************************************************************************
 normalizeRNASEQwithVST <- function(readCount, filter_thresh=1) {
 	
@@ -2209,6 +2370,67 @@ normalizeRNASEQwithVST <- function(readCount, filter_thresh=1) {
 	
 	return (transCnt)
 	
+}
+
+
+#'******************************************************************************
+#' A function to normalize RNA-Seq data with TPM
+#' readCount: RNA-Seq rawcounts in a matrix or in a data frame form
+#'            Rows are genes and columns are samples
+#'            The row names should be Entrez IDs
+#' gene_length_RDA_path: The RDA file that contains gene length information
+#'                       e.g., //isilon.c2b2.columbia.edu/ifs/archive/shares/af_lab/GTEx/RDA_Files/exon_lengths_hg19.rda
+#'                       e.g., //isilon.c2b2.columbia.edu/ifs/archive/shares/af_lab/GTEx/RDA_Files/exon_lengths_hg38.rda
+#' ref: The genome reference for the gene length information - either "hg19" or "hg38"
+#' filter_thresh: The function filters out genes that have at least one sample
+#'                with counts larger than the 'filter_thresh' value
+#'                e.g., if the 'filter_thresh' = 1, then it removes genes
+#'                that have counts <= 1 across all the samples
+#'                if 0, then there will be no filtering
+#'******************************************************************************
+normalizeRNASEQwithTPM <- function(readCount,
+                                   gene_length_RDA_path,
+                                   ref="hg19",
+                                   filter_thresh=1) {
+  
+  ### load data
+  if(ref == "hg19" && !exists("gene_lengths")) {
+    load(as.character(gene_length_RDA_path), envir = globalenv())
+    gene_lengths <- get("exon_lengths_hg19", envir = globalenv())
+  } else if(ref == "hg38" && !exists("gene_lengths")) {
+    load(as.character(gene_length_RDA_path), envir = globalenv())
+    gene_lengths <- get("exon_lengths_hg38", envir = globalenv())
+  }
+  Sys.sleep(3)
+  
+  ### filtering
+  if(filter_thresh > 0) {
+    ### Remove rubbish rows - this will decrease the number of rows
+    keep <- apply(readCount, 1, function(r){
+      return(sum(r > filter_thresh) > 0)
+    })
+    readCount <- readCount[keep,]
+  }
+  
+  ### get gene lengths for the given genes
+  gene_lens <- gene_lengths[rownames(readCount)]
+  
+  ### if there is no gene length information, remove those genes out
+  keep <- which(!is.na(gene_lens))
+  readCount <- readCount[keep,]
+  gene_lens <- gene_lens[keep]
+  
+  ### TPM normalization
+  ### normalize based on gene length first
+  normCount <- readCount
+  for(gene in rownames(normCount)) {
+    normCount[gene,] <- normCount[gene,] / gene_lens[gene]
+  }
+  ### normalize based on read depth
+  normCount <- t(t(normCount) * 1e6 / colSums(normCount))
+  
+  return(normCount)
+  
 }
 
 
